0ff3e850ca7b569af2ee5afbdf136b3c556d9739
Wahyu Tamuu <75367930+whyutams@users.noreply.github.com>
2026-03-19 09:39:24 +0800

    feat: add fitur pencarian film

--- src/scrapers/lk21.ts
+++ src/scrapers/lk21.ts
@@ -126,4 +126,55 @@ export class Lk21 {
             return main_data;
         }
     }
+
+    /**
+     * Layar kaca 21 - Cari Film
+     * @param {string} query - Kata kunci pencarian
+     * @param {number} page - Halaman pencarian (default: 1)
+     */
+    public async Search(query: string, page: number = 1): Promise<any> {
+        const endpoint = `/search?s=${encodeURIComponent(query)}`;
+        let main_data = {
+            base_url: this.BASE_URL,
+            url: this.BASE_URL + endpoint,
+            error: false,
+            data: [] as any[]
+        };
+
+        try {
+            const html = await fetchHtml(this.BASE_URL);
+            if (!html) {
+                main_data.error = true;
+                return main_data;
+            }
+
+            const $ = cheerio.load(html);
+            const search_url = $("body").attr("data-search_url");
+            const thumbnail_url = $("body").attr("data-thumbnail_url") || "https://static-jpg.lk21.party/wp-content/uploads/";
+            if (!search_url) {
+                console.error("API URL tidak ditemukan");
+                main_data.error = true;
+                return main_data;
+            }
+
+            const apiUrl = `${search_url}search.php?s=${encodeURIComponent(query)}&page=${page}`;
+            const response = await fetchHtml(apiUrl, this.BASE_URL) as any
+            if (!response || response.error || !response.data || response.data.length === 0) {
+                main_data.error = true;
+                return main_data;
+            }
+
+            main_data.data = response.data.map((item: any) => {
+                const poster = item.poster ? thumbnail_url + item.poster : null;
+                let obj = { title: item.title, rating: item.rating ? String(item.rating) : "?", year: item.year ? String(item.year) : "", duration: item.runtime || (item.season ? `S.${item.season}` : ""), pict: { sd: poster, hd: poster }, url: `${this.BASE_URL}/${item.slug}`, base_url: this.BASE_URL };
+                return obj;
+            });
+
+            return main_data;
+        } catch (error: any) {
+            console.error(error);
+            main_data.error = true;
+            return main_data;
+        }
+    }
 }
