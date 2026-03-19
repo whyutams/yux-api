9b05fbe168ac262d19cc26a506c8e582715698f7
Wahyu Tamuu <75367930+whyutams@users.noreply.github.com>
2026-03-19 20:49:00 +0800

    feat: update feature name

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { Lk21 } from "./src/scrapers/lk21.js";
 
-/* LK21 */
-export { Lk21 };
-export const lk21 = new Lk21();
+export { Lk21 }; 
+/**
+ * Layar Kaca 21
+ */
+export const LK21 = new Lk21();
