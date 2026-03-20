65272fc4354de41914fbfe20dd74c0697036853d
Wahyu Tamuu <75367930+whyutams@users.noreply.github.com>
2026-03-20 12:50:00 +0800

    feat: update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,10 @@
-export type GlobalFormat = {
+export type GlobalFormat<T = any | any[]> = {
     /** Base URL */
     base_url: string,
     /** Base URL + Endpoint */
     url: string,
     /** Status error: true/false */
     error: boolean,
-    /** @type {any | any[]} Data yang akan dikembalikan bisa berupa objek atau array */
-    data: any | any[]
+    /** @type {any | any[]} Data yang akan dikembalikan */
+    data: T
 }
