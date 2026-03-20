dff863e39195491e4da30fb20e6be82a7be84d7f
Wahyu Tamuu <75367930+whyutams@users.noreply.github.com>
2026-03-20 13:22:58 +0800

    chore: update jsdoc

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import * as lk21Scraper from "./src/scrapers/lk21.js";
 
 /**
  * Film - Layar Kaca 21
- * * @method Homepage Mengambil data konten untuk halaman utama
+ * @method Homepage Mengambil data konten untuk halaman utama
  * @method Newest Mengambil data konten untuk halaman film terbaru
  * @method Search Melakukan pencarian film berdasarkan kata kunci
  * @method Genre Mengambil daftar film berdasarkan genre
